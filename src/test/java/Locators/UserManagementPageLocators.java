3dbfb43489fccec864ca8b8e5686be0a134fcb2e
e2_Sivakumar <92861564+e2-Sivakumar98@users.noreply.github.com>
2023-03-07 11:21:51 +0530

    Update UserManagementPageLocators.java

--- src/test/java/Locators/UserManagementPageLocators.java
+++ src/test/java/Locators/UserManagementPageLocators.java
@@ -1,5 +1,6 @@
 package Locators;
 
+import java.io.IOException;
 import java.util.List;
 
 import org.openqa.selenium.WebElement;
@@ -8,87 +9,113 @@ import org.openqa.selenium.support.PageFactory;
 
 import Base.Base_class;
 
-public class UserManagementPageLocators extends Base_class{
-	
-	
+public class UserManagementPageLocators extends Base_class {
+
 	public UserManagementPageLocators() {
 		PageFactory.initElements(driver, this);
-		// TODO Auto-generated constructor stub
 	}
+
+	@FindBy(xpath = "//button[@type='button']")
+	private WebElement menuCancelButton;
 
 	@FindBy(xpath = "//div[text()='System Administration ']")
 	private WebElement sytemAdministration;
-	
+
 	@FindBy(xpath = "//div[text()='User Administration ']")
 	private WebElement userAdministration;
-	
+
 	@FindBy(xpath = "//a[text()='User Management']")
 	private WebElement userManagement;
-	
-	public WebElement getUserManagementTitleName() {
-		return userManagementTitleName;
-	}
 
 	@FindBy(xpath = "//div[text()='User Management']")
 	private WebElement userManagementTitleName;
-	
-	@FindBy(xpath = "(//button[@type='button'])[2]")
+
+	@FindBy(xpath = "//span[text()='Add User']")
 	private WebElement addUserButton;
-	
+
 	@FindBy(xpath = "//label[text()='Title']")
 	private WebElement title;
-	
+
 	@FindBy(id = "firstName")
 	private WebElement firstName;
-	
+
 	@FindBy(id = "lastName")
 	private WebElement lastName;
-	
+
 	@FindBy(id = "userContact")
 	private WebElement contactNumber;
-	
+
 	@FindBy(id = "userEmail")
 	private WebElement emailAddress;
-	
+
 	@FindBy(id = "userPassword")
 	private WebElement password;
-	
+
 	@FindBy(id = "userConfirmPassword")
 	private WebElement confirmPassword;
-	
+
 	@FindBy(xpath = "(//button[@class='MuiButtonBase-root MuiIconButton-root'])[2]")
 	private WebElement passwordMaskIcon;
-	
+
 	@FindBy(xpath = "(//button[@class='MuiButtonBase-root MuiIconButton-root'])[3]")
 	private WebElement confirmPasswordMaskIcon;
 
 	@FindBy(xpath = "//div[@aria-labelledby='userType']")
 	private WebElement userRole;
-	
+
 	@FindBy(xpath = "(//li[@role='option'])[1]")
-	private WebElement userRolePrivilege;
-	
+	private WebElement userRolePrivilegeAdmin;
+
+	@FindBy(xpath = "(//li[@role='option'])[2]")
+	private WebElement userRolePrivilegeStaff;
+
+	@FindBy(xpath = "//p[text()='Minimum 6 Characters Required.']")
+	private WebElement minimum6CharaterErrorMessage;
+
+	@FindBy(xpath = "//p[text()='Password does not match']")
+	private WebElement passworDoesnotMatchErrorMessage;
+
+	@FindBy(xpath = "//p[text()='Enter valid email address.']")
+	private WebElement validEmailErrorMessage;
+
 	@FindBy(xpath = "(//input[@type='radio'])[2]")
 	private WebElement radioButton;
 
 	@FindBy(xpath = "(//div[@role='button'])[3]")
 	private WebElement partnerNameDropdown;
-	
+
 	@FindBy(xpath = "//li[text()='1QA Tester']")
 	private WebElement selectPartnerName;
-	
+
 	@FindBy(xpath = "//button[@type='submit']")
 	private WebElement submitButton;
-	
+
 	@FindBy(xpath = "(//button[@class='MuiButtonBase-root MuiIconButton-root'])[1]")
 	private WebElement addUserDialogPageCancelButton;
-	
+
 	@FindBy(xpath = "//div[@role='alert']")
 	private WebElement emailIdAlreadyExists;
-	
-	//This is for delete icon button xpath
-	//(//*[local-name()='svg'and @class='MuiSvgIcon-root'])[4]
-	
+
+	@FindBy(xpath = "//p[text()='This field is required.']")
+	private WebElement fieldRequriedErrormessage;
+
+	public WebElement getMenuCancelButton() {
+		return menuCancelButton;
+	}
+
+	@FindBy(xpath = "//span[text()='User created successfully']")
+	private WebElement userCreatedSuccessMessage;
+
+	public WebElement getUserCreatedSuccessMessage() {
+		return userCreatedSuccessMessage;
+	}
+
+	// This is for delete icon button xpath
+	// (//*[local-name()='svg'and @class='MuiSvgIcon-root'])[4]
+	public WebElement getUserManagementTitleName() {
+		return userManagementTitleName;
+	}
+
 	public WebElement getSytemAdministration() {
 		return sytemAdministration;
 	}
@@ -145,8 +172,12 @@ public class UserManagementPageLocators extends Base_class{
 		return userRole;
 	}
 
-	public WebElement getUserRolePrivilege() {
-		return userRolePrivilege;
+	public WebElement getUserRolePrivilegeAdmin() {
+		return userRolePrivilegeAdmin;
+	}
+
+	public WebElement getUserRolePrivilegeStaff() {
+		return userRolePrivilegeStaff;
 	}
 
 	public WebElement getRadioButton() {
@@ -160,7 +191,7 @@ public class UserManagementPageLocators extends Base_class{
 	public WebElement getSelectPartnerName() {
 		return selectPartnerName;
 	}
-	
+
 	public WebElement getSubmitButton() {
 		return submitButton;
 	}
@@ -173,5 +204,20 @@ public class UserManagementPageLocators extends Base_class{
 		return emailIdAlreadyExists;
 	}
 
-	
+	public WebElement getMinimum6CharaterErrorMessage() {
+		return minimum6CharaterErrorMessage;
+	}
+
+	public WebElement getPassworDoesnotMatchErrorMessage() {
+		return passworDoesnotMatchErrorMessage;
+	}
+
+	public WebElement getValidEmailErrorMessage() {
+		return validEmailErrorMessage;
+	}
+
+	public WebElement getFieldRequriedErrormessage() {
+		return fieldRequriedErrormessage;
+	}
+
 }
